273501258132c4fc838394b53f2383310abc43fd
ztarbug <kronprinzfriedrich@gmx.de>
2023-09-18 21:32:29 +0200

    add file reader example

---
+++ Test/src/de/starwit/App.java
@@ -0,0 +1,36 @@
+package de.starwit;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+	
+	static Logger log = LogManager.getLogger(App.class.getName());
+
+	public static void main(String[] args) throws Exception {
+		log.info("Test");
+		
+        Path currRelativePath = Paths.get("");
+        String currAbsolutePathString = currRelativePath.toAbsolutePath().toString();
+        System.out.println("Current absolute path is - " + currAbsolutePathString);
+		
+		List<List<String>> records = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader("data/company-data.csv"))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		        String[] values = line.split(";");
+		        records.add(Arrays.asList(values));
+		    }
+		}		
+
+	}
+
+}
